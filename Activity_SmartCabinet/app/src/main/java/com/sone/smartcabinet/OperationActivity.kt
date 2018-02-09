29138f14f4aa49c5f70859f038689b6497419a40
CXDsone <34830558+CXDsone@users.noreply.github.com>
2018-02-09 14:56:14 +0800

    0209

--- Activity_SmartCabinet/app/src/main/java/com/sone/smartcabinet/OperationActivity.kt
+++ Activity_SmartCabinet/app/src/main/java/com/sone/smartcabinet/OperationActivity.kt
@@ -11,12 +11,6 @@ class OperationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operation)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
